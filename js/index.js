009c92269cd99e161b11cf5207bd58504d38ae20
Gurgen <gurgen.developer@gmail.com>
2023-04-15 23:07:11 +0400

    ff

--- js/index.js
+++ js/index.js
@@ -1,7 +1,18 @@
 (function(){
 
 
+    var btns = document.querySelectorAll('.form-check-input');
+    for (var i = 0; i < btns.length; i++) {
+        btns[i].addEventListener('click', function (event) {
 
+            console.log('clicked');
+            console.log(event.target.closest(`tr`).style);
+            event.target.closest(`tr`).classList.toggle("myclass");
+
+
+
+        }, false);
+    }
 
 
 
